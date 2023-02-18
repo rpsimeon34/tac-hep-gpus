5d210f6417271d092cf75db1e07d977133992938
Ryan Simeon <rsimeon@wisc.edu>
2023-02-18 15:56:44 -0600

    Exercise 3 done week 3

--- week3/mult_matrix.cu
+++ week3/mult_matrix.cu
@@ -20,23 +20,31 @@ const float B_val = 2.0f;
 
 // Square matrix multiplication on CPU : C = A * B
 void matrix_mul_cpu(const float *A, const float *B, float *C, int size) {
-  //FIXME:
-  // 
+    // assume matrices are NxN
+    float sum;
+    for (int i=0;i<size;i++) {
+        for (int j=0;j<size;j++) {
+            sum = 0;
+            for (int k=0;k<size;k++) {
+                sum += A[i*size+k]*B[k*size+j];
+            }
+            C[i*size+j] = sum;
+        }
+    }
 }
 
 // Square matrix multiplication on GPU : C = A * B
 __global__ void matrix_mul_gpu(const float *A, const float *B, float *C, int size) {
 
-    //FIXME:
     // create thread x index
     // create thread y index
-    idx = ;
-    idy = ;
+    int idx = blockIdx.x*blockDim.x + threadIdx.x;
+    int idy = blockIdx.y*blockDim.y + threadIdx.y;
     // Make sure we are not out of range
-    if ((idx < FIXME) && (idy < FIXME)) {
+    if ((idx < size) && (idy < size)) {
         float temp = 0;
         for (int i = 0; i < size; i++){
-            //FIXME : Add dot product of row and column
+            temp += A[idy*size+i]*B[i*size+idx];
         }
         C[idy*size+idx] = temp;                    
     }
@@ -48,10 +56,11 @@ int main() {
     float *h_A, *h_B, *h_C, *d_A, *d_B, *d_C;
 
     // These are used for timing
-    clock_t t0, t1, t2, t3;
+    clock_t t0, t1, t2, t3, t4, t5;
     double t1sum=0.0;
     double t2sum=0.0;
     double t3sum=0.0;
+    double t4sum=0.0;
 
     // start timing
     t0 = clock();
@@ -75,32 +84,56 @@ int main() {
     // Allocate device memory and copy input data from host to device
     cudaMalloc(&d_A, DSIZE*DSIZE*sizeof(float));
     //FIXME:Add all other allocations and copies from host to device
+    cudaMalloc(&d_B, DSIZE*DSIZE*sizeof(float));
+    cudaMalloc(&d_C, DSIZE*DSIZE*sizeof(float));
+    cudaCheckErrors("Error while allocating device memory");
+
+    cudaMemcpy(d_A, h_A, DSIZE*DSIZE*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, DSIZE*DSIZE*sizeof(float), cudaMemcpyHostToDevice);
+    cudaCheckErrors("Error while copying from host to device");
   
 
     // Launch kernel
     // Specify the block and grid dimentions 
-    dim3 block(,);  //FIXME
-    dim3 grid(,); //FIXME
+    dim3 block(32,32);  //FIXME
+    dim3 grid((DSIZE+31)/32,(DSIZE+31/32)); //FIXME
+    t4 = clock();
     matrix_mul_gpu<<<grid, block>>>(d_A, d_B, d_C, DSIZE);
+    cudaCheckErrors("Error while launching the kernel");
+    t5 = clock();
+    t4sum = ((double)(t5-t4))/CLOCKS_PER_SEC;
+    printf("Executing kernel took %f seconds\n", t4sum);
 
     // Copy results back to host
     cudaMemcpy(h_C, d_C, DSIZE*DSIZE*sizeof(float), cudaMemcpyDeviceToHost);
+    cudaCheckErrors("Error while copying from device to host");
 
     // GPU timing
     t2 = clock();
     t2sum = ((double)(t2-t1))/CLOCKS_PER_SEC;
-    printf ("Done. Compute took %f seconds\n", t2sum);
+    printf ("Done. GPU compute took %f seconds\n", t2sum);
+    printf("%f seconds of GPU compute due to data transfer\n", t2sum-t4sum);
 
     // FIXME
     // Excecute and time the cpu matrix multiplication function
+    matrix_mul_cpu(h_A, h_B, h_C, DSIZE);
 
     // CPU timing
     t3 = clock();
     t3sum = ((double)(t3-t2))/CLOCKS_PER_SEC;
-    printf ("Done. Compute took %f seconds\n", t3sum);
+    printf ("Done. CPU compute took %f seconds\n", t3sum);
+
+    printf("C[0,0] is %f, expected %f\n",h_C[0],DSIZE*A_val*B_val);
 
     // FIXME
     // Free memory 
+    free(h_A);
+    free(h_B);
+    free(h_C);
+
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
     
     return 0;
 
